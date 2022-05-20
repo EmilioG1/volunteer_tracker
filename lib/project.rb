9d0e8bf317ef91116b2676c5297052b7406cc115
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-20 09:17:42 -0700

    pass second id test

--- lib/project.rb
+++ lib/project.rb
@@ -10,6 +10,20 @@ class Project
     @id = attributes.fetch(:id)
   end
 
-  
+  def self.all 
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |proj|
+      title = proj.fetch("title")
+      id = proj.fetch("id").to_i
+      projects.push(Project.new({:title => name, :id => id}))
+    end
+    projects
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{title}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
 
 end
