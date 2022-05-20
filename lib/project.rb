968c0b890b35901c9cb4ca17e8817be238a7fe74
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-20 08:47:02 -0700

    set up database

---
+++ lib/project.rb
@@ -0,0 +1,6 @@
+#As a non-profit employee, I want to view, add, update and delete projects.
+#As a non-profit employee, I want to add volunteers to a project.
+
+class Project
+  
+end
---
+++ lib/volunteer.rb
@@ -0,0 +1,7 @@
+#As a non-profit employee, I want to view and add volunteers.
+#As a non-profit employee, I want to add volunteers to a project.
+
+class Volunteer
+
+  
+end
