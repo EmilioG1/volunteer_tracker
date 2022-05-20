15dc4934fb5a3aa46c1171d63ad2730286f25b5e
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-20 08:54:52 -0700

    pass title test

--- lib/project.rb
+++ lib/project.rb
@@ -2,5 +2,14 @@
 #As a non-profit employee, I want to add volunteers to a project.
 
 class Project
+  attr_reader :id
+  attr_accessor :title
+
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+
   
+
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,93 +1,57 @@
-require "spec_helper"
+# require "spec_helper"
 
-describe Project do
-  describe '#title' do
-    it 'returns the project title' do
-      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      expect(project.title).to eq 'Teaching Kids to Code'
-    end
-  end
+# describe Volunteer do
+#   describe '#name' do
+#     it 'returns the name of the volunteer' do
+#       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+#       expect(test_volunteer.name).to eq 'Jane'
+#     end
+#   end
 
-  context '#id' do
-    it 'returns the id of the project before saving project' do
-      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      expect(project.id).to eq nil
-    end
+#   describe '#project_id' do
+#     it 'returns the project_id of the volunteer' do
+#       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+#       expect(test_volunteer.project_id).to eq 1
+#     end
+#   end
 
-    it 'returns the id of the project after saving project' do
-      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      project.save
-      expect(project.id).to be_an_instance_of Integer
-    end
-  end
+#   describe '#==' do
+#     it 'checks for equality based on the name of a volunteer' do
+#       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+#       volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+#       expect(volunteer1 == volunteer2).to eq true
+#     end
+#   end
 
-  describe '#==' do
-    it 'is the same project if two projects have the same title' do
-      project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      project2 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      expect(project1 == project2).to eq true
-    end
-  end
+#   context '.all' do
+#     it 'is empty to start' do
+#       expect(Volunteer.all).to eq []
+#     end
 
-  context '.all' do
-    it 'is empty to start' do
-      expect(Project.all).to eq []
-    end
+#     it 'returns all volunteers' do
+#       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+#       volunteer1.save
+#       volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
+#       volunteer2.save
+#       expect(Volunteer.all).to eq [volunteer1, volunteer2]
+#     end
+#   end
 
-    it 'returns all projects' do
-      project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      project1.save
-      project2 = Project.new({:title => 'Teaching Ruby to Kids', :id => nil})
-      project2.save
-      expect(Project.all).to eq [project1, project2]
-    end
-  end
+#   describe '#save' do
+#     it 'adds a volunteer to the database' do
+#       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+#       volunteer1.save
+#       expect(Volunteer.all).to eq [volunteer1]
+#     end
+#   end
 
-  describe '#save' do
-    it 'saves a project to the database' do
-      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      project.save
-      expect(Project.all).to eq [project]
-    end
-  end
-
-  describe '.find' do
-    it 'returns a project by id' do
-      project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      project1.save
-      project2 = Project.new({:title => 'Teaching Ruby to Kids', :id => nil})
-      project2.save
-      expect(Project.find(project1.id)).to eq project1
-    end
-  end
-
-  describe '#volunteers' do
-    it 'returns all volunteers for a specific project' do
-      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      project.save
-      volunteer1 = Volunteer.new({:name => 'Jasmine', :project_id => project.id, :id => nil})
-      volunteer1.save
-      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => project.id, :id => nil})
-      volunteer2.save
-      expect(project.volunteers).to eq [volunteer1, volunteer2]
-    end
-  end
-
-  describe '#update' do
-    it 'allows a user to update a project' do
-      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      project.save
-      project.update({:title => 'Teaching Ruby to Kids', :id => nil})
-      expect(project.title).to eq 'Teaching Ruby to Kids'
-    end
-  end
-
-  context '#delete' do
-    it 'allows a user to delete a project' do
-      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      project.save
-      project.delete
-      expect(Project.all).to eq []
-    end
-  end
-end
+#   describe '.find' do
+#     it 'returns a volunteer by id' do
+#       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+#       volunteer1.save
+#       volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
+#       volunteer2.save
+#       expect(Volunteer.find(volunteer1.id)).to eq volunteer1
+#     end
+#   end
+# end
