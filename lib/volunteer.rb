305a97b0c6c3948583f147c53cef07771589598f
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-20 11:31:43 -0700

    pass vol compare test

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -12,6 +12,22 @@ class Volunteer
     @project_id = attributes.fetch(:project_id)
   end
 
+  def ==(vol_to_compare)
+    (self.name == vol_to_compare.name) && (self.project_id == vol_to_compare.project_id) && (self.id == vol_to_compare.id)
+  end
+
+  # def self.all
+  #   returned_trains = DB.exec("SELECT * FROM trains;")
+  #   trains = []
+  #   returned_trains.each() do |train|
+  #     name = train.fetch("name")
+  #     id = train.fetch("id").to_i
+  #     time_id = train.fetch("time_id").to_i
+  #     trains.push(Train.new({:name => name, :id => id, :time_id => time_id}))
+  #   end
+  #   trains
+  # end
+
 
   def save 
     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{name}', #{project_id}) RETURNING id;")
